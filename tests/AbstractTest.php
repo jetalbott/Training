048f7cc67cf764b9cb73dd4d01d8d5fa8ea557aa
John Talbott <talbott.je@gmail.com>
2013-11-08 22:01:01 -0800

    Creating AbstractTest Test Class

---
+++ tests/AbstractTest.php
@@ -0,0 +1,26 @@
+<?php
+
+Class AbstractTest extends PHPUnit_Extensions_Database_TestCase
+{
+    /**
+     * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection
+     */
+    public function getConnection()
+    {
+        $config   = Config::get(Config::DATA_STORE);
+        $dsn      = $config[Config::DATA_STORE_DSN];
+        $username = $config[Config::DATA_STORE_USERNAME];
+        $password = $config[Config::DATA_STORE_PASSWORD];
+        $pdo = new PDO($dsn, $username, $password);
+
+        return $this->createDefaultDBConnection($pdo);
+    }
+
+    /**
+     * @return PHPUnit_Extensions_Database_DataSet_IDataSet
+     */
+    public function getDataSet()
+    {
+        return $this->createMySQLXMLDataSet('/path/to/file.xml');
+    }
+}
