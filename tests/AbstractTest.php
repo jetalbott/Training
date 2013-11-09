42a178db9e74ea6fd9c7d558eaa611d84e11bdc4
Bob Van Dell <robert.vandell@bloomboard.com>
2013-11-09 00:12:31 -0800

    Allowing truncation.

--- tests/AbstractTest.php
+++ tests/AbstractTest.php
@@ -1,14 +1,28 @@
 <?php
 
-Abstract Class AbstractTest extends PHPUnit_Extensions_Database_TestCase
+abstract class AbstractTest extends PHPUnit_Extensions_Database_TestCase
 {
+    public function getSetUpOperation()
+    {
+        // whether you want cascading truncates
+        // set false if unsure
+        $cascadeTruncates = false;
+
+        return new PHPUnit_Extensions_Database_Operation_Composite(array
+            (
+                new PHPUnit_Extensions_Database_Operation_MySQL55Truncate($cascadeTruncates),
+                PHPUnit_Extensions_Database_Operation_Factory::INSERT()
+            ));
+    }
+
     /**
+     *
      * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection
      */
     public function getConnection()
     {
-        $config   = Config::get(Config::DATA_STORE);
-        $dsn      = $config[Config::DATA_STORE_DSN];
+        $config = Config::get(Config::DATA_STORE);
+        $dsn = $config[Config::DATA_STORE_DSN];
         $username = $config[Config::DATA_STORE_USERNAME];
         $password = $config[Config::DATA_STORE_PASSWORD];
         $pdo = new PDO($dsn, $username, $password);
@@ -17,6 +31,7 @@ Abstract Class AbstractTest extends PHPUnit_Extensions_Database_TestCase
     }
 
     /**
+     *
      * @return PHPUnit_Extensions_Database_DataSet_IDataSet
      */
     public function getDataSet()
